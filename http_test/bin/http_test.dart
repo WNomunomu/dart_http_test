4b84758c82d80a0aad2e47e1573f3685ae4710ea
WNomunomu <kohsei.nmr@gmail.com>
2024-01-11 11:27:22 +0900

    separate function

--- http_test/bin/http_test.dart
+++ http_test/bin/http_test.dart
@@ -1,24 +1,24 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-void main() async {
-
-  final apiUrl = 'https://script.google.com/macros/s/AKfycbxr4ka9zuGOTcjx1LmM6-94zfD-QYestVrzgwSXYBgQ2gVxe7FCMZ6RxMaz3uWLdVA-/exec';
+Future<Object> fetchData() async {
+  final apiUrl = 'https://script.google.com/macros/s/AKfycby7o065pPg70rQxnPG50EdyrO7f8VbdwXvZ2KPVFYismIv1RzhYdUzf4rxGljYN1Z66/exec';
 
   try {
     final response = await http.get(Uri.parse(apiUrl));
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> jsonData = json.decode(response.body);
-
-      print(jsonData);
+      return jsonData;
+    } else {
+      return response.statusCode;
     }
-    else {
-      print(response.statusCode);
-    }
+  } catch (err) {
+    return err;
   }
-  catch (err) {
-    print(err);
-  }
+}
 
+void main() async {
+  Future<Object> jsonData = fetchData();
+  print(await jsonData);
 }
